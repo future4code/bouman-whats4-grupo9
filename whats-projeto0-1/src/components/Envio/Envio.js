f09e0b78c513057cf6877e998ba12aeaff7b230e
Soter Padua <soter19@gmail.com>
2019-12-02 13:12:04 -0300

    Corrigindo entrega

---
+++ whats-projeto0-1/src/components/Envio/Envio.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Envio.css';
+
+class Envio extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            userNameShow: props.user,
+            mensagem: props.mensagem
+        }
+    }
+
+
+    render () {
+        return (
+            <div>
+    <p>{this.state.user}{this.state.mensagem}</p>
+
+            </div>
+        )
+    }
+
+
+
+}
+
+
+
+export default Envio
